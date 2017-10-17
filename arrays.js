7a2fc3dbf0be630f48cb9db1714bc1b9448f2968
bruce leong <brucewleong@gmail.com>
2017-10-17 01:56:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
+  array.unshift(element)
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
